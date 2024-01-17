3932095107820febcaa5ab6230aa2539b11054be
vigge-vigge <victorakande847@gmail.com>
2024-01-17 17:33:12 +0100

    first commit

---
+++ function.py
@@ -0,0 +1,18 @@
+numbers = [6, 7, 2, 9, 1]
+
+# def add(num):
+#     print(sum(num))
+
+def add(li_nu):
+    res = 0
+    for item in li_nu:
+        res = res + item
+    return res
+
+
+print(add(numbers))
+
+# add(numbers)
+
+# total = sum(numbers)
+# print(total)
